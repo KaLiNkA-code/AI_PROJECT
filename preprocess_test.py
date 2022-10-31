00d6be6bb8bf9056f13c46d4f9928053f09b067a
Egor <777egorkalinin777@gmail.com>
2022-10-31 21:19:39 +0300

    class_work

--- preprocess_test.py
+++ preprocess_test.py
@@ -2,10 +2,10 @@ from sklearn import preprocessing
 
 
 def preprocess(DataSet):
-    feature_names = DataSet.columns.tolist() 
-    for column in feature_names: 
+    feature_names = DataSet.columns.tolist()
+    for column in feature_names:
         print(column)
-    DataSet[column].value_counts(dropna=False) 
-    DataSet['Annual Income (k$)'] = preprocessing.normalize(DataSet['Annual Income (k$)'].reshape(-1, 1))
-    DataSet['Spending Score (1-100)'] = preprocessing.normalize(DataSet['Spending Score (1-100)'].reshape(-1, 1))
+    DataSet[column].value_counts(dropna=False)
+    DataSet["normalized_income"] = preprocessing.normalize(DataSet["Annual Income (k$)"]).flatten()
+    DataSet["normalized_score"] = preprocessing.normalize(DataSet["Spending Score (1-100)"]).flatten()
     return DataSet
